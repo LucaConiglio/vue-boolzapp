3519d0e24e1cb5bedec7b13347c9bedd28d7cc31
LucaConiglio <lucaconiglio.92@gmail.com>
2022-11-25 19:50:03 +0100

    da rifinire qualcosa

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ createApp({
             {
               date: '20/03/2020 16:35:00',
               message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-              status: 'received'
+              status: 'sent'
             }
             ],
           },
@@ -90,25 +90,135 @@ createApp({
             }
             ],
           },
+          {
+            name: 'Ludovico',
+            avatar: 'img/avatar_5.jpg',
+            visible: true,
+
+            messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'Lo sai che parto?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Si, ma preferirei che tu restassi',
+              status: 'received'
+            }
+            ],
+          },
+          {
+            name: 'Daniela',
+            avatar: 'img/avatar_6.jpg',
+            visible: true,
+
+            messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'andiamo a correre domani mattina?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Ma sei pazza non ci penso proprio devo finire dei lavori per gian luca',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:55:00',
+              message: 'Va bene non ti chiamerò più',
+              status: 'sent'
+            }
+            ],
+          },
+          {
+            name: 'Giovanni',
+            avatar: 'img/avatar_7.jpg',
+            visible: true,
+
+            messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'Ceniamo insieme a Mirko e valentina Stasera?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Ma c é la partita questa sera te l eri dimenticato?',
+              status: 'received'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Hai pienamente ragione mi era sfuggito di mente che faccio disdico?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Niente non ti preoccupare per una volta posso anche saltarla',
+              status: 'received'
+            }
+            ],
+          },
+          {
+            name: 'Ermegildo',
+            avatar: 'img/avatar_8.jpg',
+            visible: true,
+
+            messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'A che ora pensi di liberarti?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Verso le 19 credo vai a fare la spesa se hai terminato di lavorare!',
+              status: 'received'
+            }
+            ],
+          },
           
           
           ],
 
           currentIndex : 0,
-        
+          newMessage : '',
+          createdOra : new Date().toLocaleTimeString(),
+          createdData : new Date().toLocaleDateString(),
+          dataVisible : true,
 };
   },
   methods : {
 
     onChatClick (index){
-        this.currentIndex = index
-    }
+        this.currentIndex = index;
+        this.dataVisible = false;
+    },
+
+    onInputMessage (currentIndex) {
+      this.users[currentIndex].messages.push({
+        date : (this.createdData+ ' ' + this.createdOra ),
+        message : this.newMessage,
+        status : 'sent'
+      });
+
+      setTimeout(() => {
+        
+        this.users[currentIndex].messages.push({
+          date : ( this.createdData + ' ' + this.createdOra ),
+          message : 'Va benissimo.... OK',
+          status : 'received'
+        });
+        
+      }, 1500);
+
+      this.newMessage= '';
+
+    },
+
+
 
 
 
   },
   mounted () {
-
+    
     
   }
 }).mount("#app");
